1cdfa2672790418c47f29d77002840fdfdf52aa0
Connor Doyle <connor@mesosphere.io>
2014-09-18 11:34:00 -0700

    Enable cross-building for Scala 2.10 and 2.11

--- project/build.scala
+++ project/build.scala
@@ -17,7 +17,7 @@ object MesosUtilsBuild extends Build {
 
   lazy val baseSettings = Defaults.defaultSettings ++ Seq (
     organization := "mesosphere",
-    scalaVersion := "2.10.4",
+    crossScalaVersions := Seq("2.10.4", "2.11.2"),
     scalacOptions in Compile ++= Seq("-encoding", "UTF-8", "-target:jvm-1.6", "-deprecation", "-feature", "-unchecked", "-Xlog-reflective-calls", "-Xlint"),
     javacOptions in Compile ++= Seq("-encoding", "UTF-8", "-source", "1.6", "-target", "1.6", "-Xlint:unchecked", "-Xlint:deprecation"),
     resolvers ++= Seq(
