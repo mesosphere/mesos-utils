d821f507ea523a664f349c68e0ff4fa0a9e6dcad
Connor Doyle <connor@mesosphere.io>
2014-09-24 22:48:58 -0700

    Updated Mesos dependency to 0.20.1

--- project/build.scala
+++ project/build.scala
@@ -69,7 +69,7 @@ object Dependencies {
 object Dependency {
   object V {
     // runtime deps versions
-    val Mesos = "0.20.0"
+    val Mesos = "0.20.1"
 
     // test deps versions
     val ScalaTest = "2.2.1"
