94797db376f7323c622a21591c522266d6e36a2c
Jason Swartz <dev@bks2.com>
2015-03-13 17:56:08 -0700

    Now using async non-blocking IO where possible. Improved error checking, especially for connection issues.

--- src/main/scala/mesosphere/util/ZookeeperDirectState.scala
+++ src/main/scala/mesosphere/util/ZookeeperDirectState.scala
@@ -4,16 +4,15 @@ import java.io.IOException
 import java.lang.Boolean
 import java.util
 import java.util.UUID
-import java.util.concurrent.{ Callable, Future => JFuture, FutureTask }
+import java.util.concurrent.{ Callable, Future => JFuture, FutureTask, TimeoutException }
 
 import scala.concurrent.{ ExecutionContext, Future => SFuture, Promise }
 
 import com.google.protobuf.ByteString
 import mesosphere.mesos.protos.StateProtos
 import org.apache.curator.framework.CuratorFramework
-import org.apache.curator.framework.api.{ BackgroundCallback, CuratorEvent }
+import org.apache.curator.framework.api._
 import org.apache.curator.utils.ZKPaths
-import org.apache.log4j.Logger
 import org.apache.mesos.state.{ State, Variable }
 
 /**
@@ -32,6 +31,7 @@ import org.apache.mesos.state.{ State, Variable }
   * The State methods return a subtype of Java's Future that implements ScalaFutureSupport
   * and contains a Scala Future which may eventually contain the expected value.
   */
+
 trait ScalaFutureSupport[A] {
   def future: SFuture[A]
 }
@@ -39,59 +39,83 @@ trait ScalaFutureSupport[A] {
 class ZookeeperDirectState(zNode: String, client: CuratorFramework, implicit val context: ExecutionContext)
     extends State with ZKBackgroundPromiseSupport {
 
-  private val log = Logger.getLogger(getClass.getName)
   private val MAX_ZK_SIZE_IN_BYTES = 0xfffff
 
   /**
-    * Fetches the data from ZK. Returns an instance of ScalaFutureSupport type containing a Scala-Future
+    * Fetches the data from ZK.
+    *
+    * Cast this to ScalaFutureSupport to access the valid Scala future, as the Java future is inoperative.
     */
   override def fetch(name: String): JFuture[Variable] = {
-    handleFetch(name, new GetDataCallbackPromise(name)).asScalaFutureWrapper
+    val result: SFuture[Variable] = connectionFailedFuture getOrElse handleFetch(name)
+    result.asScalaFutureWrapper
   }
 
   /**
     * Marshall a variable to an Entry and store it in ZK.
-    * Returns an instance of ScalaFutureSupport type containing a Scala-Future
+    *
+    * Cast this to ScalaFutureSupport to access the valid Scala future, as the Java future is inoperative.
     */
   override def store(variable: Variable): JFuture[Variable] = {
 
     if (variable.value().size > MAX_ZK_SIZE_IN_BYTES) {
       throw new IOException("Serialized data is too big (> 1 MB)")
     }
-    handleStore(variable.asLocal).asScalaFutureWrapper
+
+    val result: SFuture[Variable] = connectionFailedFuture getOrElse handleStore(variable.asLocal)
+    result.asScalaFutureWrapper
   }
 
   /**
     * Remove the variable from ZK. Returns false if deletion was not necessary.
-    * Returns an instance of ScalaFutureSupport type containing a Scala-Future
+    *
+    * Cast this to ScalaFutureSupport to access the valid Scala future, as the Java future is inoperative.
     */
   override def expunge(variable: Variable): JFuture[Boolean] = {
-    handleExpunge(variable.asLocal).asScalaFutureWrapper
+    val result: SFuture[Boolean] = connectionFailedFuture getOrElse handleExpunge(variable.asLocal)
+    result.asScalaFutureWrapper
   }
 
   /**
     * Returns the children of our ZK node.
-    * Returns an instance of ScalaFutureSupport type containing a Scala-Future
+    *
+    * Cast this to ScalaFutureSupport to access the valid Scala future, as the Java future is inoperative.
     */
   override def names: JFuture[util.Iterator[String]] = {
-    handleNames.asScalaFutureWrapper
+    val result: SFuture[util.Iterator[String]] = connectionFailedFuture getOrElse handleNames
+    result.asScalaFutureWrapper
+  }
+
+  /**
+    * If Zookeeper is connected returns None, otherwise returns a failed future
+    * with a TimeoutException.
+    */
+  private def connectionFailedFuture[A]: Option[SFuture[A]] = {
+    client.getZookeeperClient.isConnected match {
+      case true  => None
+      case false => Some(SFuture.failed[A](new TimeoutException("Could not connect to Zookeeper")))
+    }
   }
 
   /**
     * Fetches the data from ZK. Returns an instance of ScalaFutureSupport type containing a Scala-Future.
     * Uses a given callback to convert Curator's result to a Promise and then a Scala Future.
     */
-  def handleFetch(name: String, callback: GetDataCallbackPromise): SFuture[Variable] = {
+  private def handleFetch(name: String): SFuture[LocalVariable] = {
 
     if (name == null) {
       throw new RuntimeException("Whoah, name is null!")
     }
 
-    exists(name) match {
-      case java.lang.Boolean.TRUE  => client.getData.inBackground(callback).forPath(fullPath(name))
-      case java.lang.Boolean.FALSE => callback.success(new LocalVariable(name))
+    exists(name) flatMap {
+      case x if x  => handleFetchForExistingPath(name)
+      case x if !x => SFuture.successful(new LocalVariable(name))
     }
+  }
 
+  private def handleFetchForExistingPath(name: String): SFuture[LocalVariable] = {
+    val callback = new GetDataCallbackPromise(name)
+    client.getData.inBackground(callback).forPath(fullPath(name))
     callback.future
   }
 
@@ -101,56 +125,38 @@ class ZookeeperDirectState(zNode: String, client: CuratorFramework, implicit val
     * First fetches a variable and then flat maps its Scala future
     * as the second argument to store(LocalVariable, LocalVariable)
     * so it can ensure the right version is being updated.
-    *
-    * Returns an instance of ScalaFutureSupport type containing a Scala-Future
-    */
-  private def handleStore(lv: LocalVariable): SFuture[Variable] = {
-
-    implicit class JFutureToSFuture[A](f: JFuture[A]) {
-      def asScala: SFuture[A] = f match {
-        case sf: ScalaFutureSupport[A] => sf.future
-      }
-    }
-
-    // Grab the latest persisted variable and flatMap its future to store()'s future
-    val persistedVariableFuture: SFuture[Variable] = fetch(lv.name).asScala
-    val storeFuture: SFuture[Variable] = persistedVariableFuture.flatMap { pv: Variable =>
-      Option(handleStore(lv, pv.asLocal)) getOrElse SFuture(null)
-    }
-
-    storeFuture
-  }
-
-  /**
-    * Marshall a variable to an Entry and store it in ZK.
     * Compares the variable with the latest persisted one to prevent an obsolete
     * variable from being persisted.
     *
-    * Returns null if the new veriable cannot be persisted
+    * Returns a future with the variable if the versions match, else a future with a null
     */
-  private def handleStore(lv: LocalVariable, persistedVariable: LocalVariable): SFuture[Variable] = {
+  private def handleStore(lv: LocalVariable): SFuture[LocalVariable] = {
 
-    val inputMatchesPersistedVersion: Boolean = persistedVariable.storedUuid == lv.storedUuid
-
-    if (inputMatchesPersistedVersion) {
-
-      val entryData = StateProtos.Entry.newBuilder()
-        .setName(lv.name)
-        .setUuid(ByteString.copyFromUtf8(UUID.randomUUID().toString))
-        .setValue(ByteString.copyFrom(lv.value()))
-        .build()
-        .toByteArray
-
-      mkDir(Some(lv.name))
-
+    def doStore(pv: LocalVariable) = {
       val callback = new SetDataCallbackPromise(lv.name)
+      val entryData = marshall(lv)
       client.setData().inBackground(callback).forPath(fullPath(lv.name), entryData)
-      callback.future
+      callback.future.map(_ => parseLocalVariable(lv.name, entryData))
     }
-    else {
-      log.error(s"Attempt was made to persist ${lv.name} from obsolete version")
-      null
-    }
+
+    for {
+      pv <- handleFetch(lv.name)
+      pathsCreated <- mkdirCompletePath(fullPath(lv.name))
+      versionsMatch = lv.storedUuid == pv.storedUuid
+      result <- if (versionsMatch) doStore(pv) else SFuture.successful(null)
+    } yield result
+  }
+
+  /**
+    * Convert a local variable into an Entry and then marshall it to a byte array
+    */
+  private def marshall(v: LocalVariable): Array[Byte] = {
+    StateProtos.Entry.newBuilder()
+      .setName(v.name)
+      .setUuid(ByteString.copyFromUtf8(UUID.randomUUID().toString))
+      .setValue(ByteString.copyFrom(v.value()))
+      .build()
+      .toByteArray
   }
 
   /**
@@ -158,16 +164,15 @@ class ZookeeperDirectState(zNode: String, client: CuratorFramework, implicit val
     * Returns an instance of ScalaFutureSupport type containing a Scala-Future
     */
   private def handleExpunge(lv: LocalVariable): SFuture[Boolean] = {
+    exists(lv.name) flatMap {
+      case x if x  => handleExpungeForExistingPath(lv)
+      case x if !x => SFuture.successful(false)
+    }
+  }
 
+  private def handleExpungeForExistingPath(lv: LocalVariable): SFuture[Boolean] = {
     val callback = new ExpungeCallbackPromise()
-
-    if (exists(lv.name)) {
-      client.delete().inBackground(callback).forPath(fullPath(lv.name))
-    }
-    else {
-      callback.success(false)
-    }
-
+    client.delete.inBackground(callback).forPath(fullPath(lv.name))
     callback.future
   }
 
@@ -175,7 +180,7 @@ class ZookeeperDirectState(zNode: String, client: CuratorFramework, implicit val
     * Returns the children of our ZK node.
     * Returns an instance of ScalaFutureSupport type containing a Scala-Future
     */
-  def handleNames: SFuture[util.Iterator[String]] = {
+  private def handleNames: SFuture[util.Iterator[String]] = {
     val callback = new NamesCallbackPromise
     client.getChildren.inBackground(callback).forPath(zNode)
     callback.future
@@ -183,15 +188,68 @@ class ZookeeperDirectState(zNode: String, client: CuratorFramework, implicit val
 
   private def fullPath(name: String) = s"$zNode/$name"
 
-  private def mkDir(name: Option[String] = None, zkClient: CuratorFramework = client): Unit = {
-    val path = name map fullPath getOrElse zNode
-    ZKPaths.mkdirs(zkClient.getZookeeperClient.getZooKeeper, path)
+  private def mkDir(name: String): Unit = {
+    val path = fullPath(name)
+    ZKPaths.mkdirs(client.getZookeeperClient.getZooKeeper, path)
   }
 
-  private def exists(name: String): Boolean = {
-    client.checkExists().forPath(fullPath(name)) != null
+  private def mkdirCompletePath(path: String): SFuture[Boolean] = SFuture {
+    ZKPaths.mkdirs(client.getZookeeperClient.getZooKeeper, path)
+    true
   }
 
+  // Disabled - ZooKeeper is taking these async callbacks out of order, failing them.
+  // - If there is a way to fix that, we should investigate a good way to do this
+  //
+  //  /**
+  //    * Creates empty ZK nodes for each item in slash-delimited path.
+  //    * TODO optimize by reverse-searching paths to prevent unnecessary checks
+  //    */
+  //  private def mkdirCompletePath(path: String): SFuture[Seq[Boolean]] = {
+  //
+  //    def mkNode(path: String): SFuture[Boolean] = {
+  //      println(s"Making node $path")
+  //      val callback = new CreateCallbackPromise
+  //      client.create().inBackground(callback).forPath(path)
+  //      callback.future
+  //    }
+  //
+  //    def mkNodeIfNotExists(path: String): SFuture[Boolean] = {
+  //      for {
+  //        pathExists <- existsFullPath(path)
+  //        if !pathExists
+  //        nodeFuture <- mkNode(path)
+  //      } yield nodeFuture
+  //    }
+  //
+  //    val parts = path split "/" filter (_.nonEmpty)
+  //
+  //    val nodeMakingFutures: Seq[SFuture[Boolean]] = for {
+  //      i <- 1 to parts.size
+  //      subPathItems = parts take i
+  //      subPath = subPathItems.mkString("/", "/", "")
+  //      nodeFuture = mkNodeIfNotExists(subPath)
+  //    } yield nodeFuture
+  //
+  //    SFuture.sequence(nodeMakingFutures)
+  //  }
+
+  private def exists(name: String): SFuture[Boolean] = existsFullPath(fullPath(name))
+
+  private def existsFullPath(name: String): SFuture[Boolean] = {
+    val callback = new ExistsCallbackPromise
+    client.checkExists.inBackground(callback).forPath(name)
+    callback.future
+  }
+
+  def parseLocalVariable(name: String, entryData: Array[Byte]): LocalVariable = {
+    val entry = StateProtos.Entry.parseFrom(entryData)
+    new LocalVariable(name, entry.getValue.toByteArray, Some(entry.getUuid.toStringUtf8))
+  }
+
+  /**
+    * Assists with conversion from the Variable interface to our own implementation, LocalVariable
+    */
   implicit class LocalVariableConversion(v: Variable) {
     def asLocal: LocalVariable = v match {
       case lv: LocalVariable =>
@@ -242,8 +300,8 @@ trait ZKBackgroundPromiseSupport { self: ZookeeperDirectState =>
     * and Java futures... all in one!
     */
   abstract class CallbackPromise[A] extends BackgroundCallback {
-    val prommy: Promise[A] = Promise[A]()
-    def future: SFuture[A] = prommy.future
+    protected val promise: Promise[A] = Promise[A]()
+    def future: SFuture[A] = promise.future
   }
 
   /**
@@ -253,15 +311,12 @@ trait ZKBackgroundPromiseSupport { self: ZookeeperDirectState =>
     * called as a Java Future. This allows us to pass the Scala Future directly to Marathon/Chronos
     * via the Java-Future-based State interface.
     */
-  class GetDataCallbackPromise(name: String) extends CallbackPromise[Variable] {
+  class GetDataCallbackPromise(name: String) extends CallbackPromise[LocalVariable] {
 
     override def processResult(client: CuratorFramework, event: CuratorEvent): Unit = {
-      val entry = StateProtos.Entry.parseFrom(event.getData)
-      val lv = new LocalVariable(name, entry.getValue.toByteArray, Some(entry.getUuid.toStringUtf8))
-      prommy.success(lv)
+      val lv = parseLocalVariable(name, event.getData)
+      promise.success(lv)
     }
-
-    def success(v: Variable): Unit = { prommy.success(v) }
   }
 
   /**
@@ -272,14 +327,10 @@ trait ZKBackgroundPromiseSupport { self: ZookeeperDirectState =>
     * called as a Java Future. This allows us to pass the Scala Future directly to Marathon/Chronos
     * via the Java-Future-based State interface.
     */
-  class SetDataCallbackPromise(name: String) extends CallbackPromise[Variable] {
-
-    private val eventualFetch = new GetDataCallbackPromise(name)
-
-    override def future: SFuture[Variable] = eventualFetch.future
+  class SetDataCallbackPromise(name: String) extends CallbackPromise[Boolean] {
 
     override def processResult(client: CuratorFramework, event: CuratorEvent): Unit = {
-      handleFetch(name, eventualFetch)
+      promise.success(true)
     }
   }
 
@@ -293,10 +344,10 @@ trait ZKBackgroundPromiseSupport { self: ZookeeperDirectState =>
   class ExpungeCallbackPromise extends CallbackPromise[Boolean] {
 
     override def processResult(client: CuratorFramework, event: CuratorEvent): Unit = {
-      prommy.success(true)
+      promise.success(true)
     }
 
-    def success(b: Boolean): Unit = { prommy.success(b) }
+    def success(b: Boolean): Unit = promise.success(b)
   }
 
   /**
@@ -309,7 +360,32 @@ trait ZKBackgroundPromiseSupport { self: ZookeeperDirectState =>
   class NamesCallbackPromise extends CallbackPromise[util.Iterator[String]] {
 
     override def processResult(client: CuratorFramework, event: CuratorEvent): Unit = {
-      prommy.success(event.getChildren.iterator())
+      promise.success(event.getChildren.iterator())
+    }
+  }
+
+  /**
+    * A callback for zookeeper.create()
+    */
+  class CreateCallbackPromise extends CallbackPromise[Boolean] {
+
+    override def processResult(client: CuratorFramework, event: CuratorEvent): Unit = {
+      promise.success(true)
+    }
+  }
+
+  /**
+    * A callback for zookeeper.exists() which returns true if the item exists
+    *
+    * Uses a Promise to generate a Scala Future. Also extends Java Future, but will throw an exception if
+    * called as a Java Future. This allows us to pass the Scala Future directly to Marathon/Chronos
+    * via the Java-Future-based State interface.
+    */
+  class ExistsCallbackPromise extends CallbackPromise[Boolean] {
+
+    override def processResult(client: CuratorFramework, event: CuratorEvent): Unit = {
+      val exists = event.getStat != null
+      promise.success(exists)
     }
   }
 
--- src/test/scala/mesosphere/util/ZookeeperDirectStateSpec.scala
+++ src/test/scala/mesosphere/util/ZookeeperDirectStateSpec.scala
@@ -1,60 +1,153 @@
 package mesosphere.util
 
-import scala.concurrent.{ Future, Await }
+import java.util.concurrent.{ Future => JFuture, TimeUnit, TimeoutException }
+
+import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import scala.util.Random
+import scala.concurrent.{ Await, Future }
+import scala.util.{ Failure, Random, Success }
 
 import org.apache.curator.framework.CuratorFrameworkFactory
-import org.apache.curator.retry.ExponentialBackoffRetry
+import org.apache.curator.retry.RetryNTimes
 import org.apache.curator.test.TestingServer
-import org.apache.mesos.state.{ Variable, State, ZooKeeperState }
-import scala.collection.JavaConverters._
-
-import java.util.concurrent.{ Future => JFuture, TimeUnit }
-
+import org.apache.curator.utils.DebugUtils
+import org.apache.mesos.state.{ State, Variable, ZooKeeperState }
 import org.scalatest.{ FlatSpec, Matchers }
 
-class ZookeeperDirectStateSpec extends FlatSpec with StateTesting
-    with ZookeeperTestServerSupport
+/**
+  * Tests the ZookeeperDirectState (written in Scala and using Apache Curator)
+  * against the JNI-based ZooKeeperState.
+  *
+  * Note - the JNI-related tests require that the libmesos native library
+  * be installed and pointed to by the MESOS_NATIVE_JAVA_LIBRARY env.
+  */
+class ZookeeperDirectStateSpec
+    extends FlatSpec
+    with StateTesting
+    with Matchers
+    with JFutureValues
     with RandomTestUtils {
 
-  val znode = "/" + randomString(10)
-  val timeout = 5.seconds
+  System.setProperty(DebugUtils.PROPERTY_DONT_LOG_CONNECTION_ISSUES, "false")
 
-  lazy val zkJNIState: ZooKeeperState = new ZooKeeperState(
-    testZKServerList,
-    timeout.toSeconds,
-    SECONDS,
-    znode
-  )
-
-  lazy val zkDirectState: ZookeeperDirectState = {
-
-    val client = CuratorFrameworkFactory.newClient(
-      testZKServerList,
-      2000,
-      2000,
-      new ExponentialBackoffRetry(1000, 4)
-    )
-
-    client.start()
-
-    new ZookeeperDirectState(znode, client, scala.concurrent.ExecutionContext.Implicits.global)
-  }
-
-  // Note - the JNI-related tests require that the libmesos native library
-  // be installed and pointed to by the MESOS_NATIVE_JAVA_LIBRARY env.
+  lazy val zkJNIState: ZooKeeperState = createJNIState()
+  lazy val zkDirectState: ZookeeperDirectState = createDirectState()
 
   // Run the standard suite of tests on both State implementations
   it should behave like testState(zkJNIState, "ZooKeeperState")
   it should behave like testState(zkDirectState, "ZookeeperDirectState")
 
   // Run tests to ensure the State implementations are fully compatible
-  it should behave like interstateTesting(zkDirectState, zkJNIState)
-  it should behave like interstateTesting(zkJNIState, zkDirectState)
-  it should behave like interstateTesting(zkJNIState, zkJNIState)
-  it should behave like interstateTesting(zkDirectState, zkDirectState)
+  val compatibleZNode = znode
+  val compatibleJniState = createJNIState(compatibleZNode)
+  val compatibleDirectState = createDirectState(compatibleZNode)
+  it should behave like interstateTesting(compatibleDirectState, compatibleJniState)
+  it should behave like interstateTesting(compatibleJniState, compatibleDirectState)
+  it should behave like interstateTesting(compatibleJniState, compatibleJniState)
+  it should behave like interstateTesting(compatibleDirectState, compatibleDirectState)
 
+  // Zookeeper-specific tests
+
+  "ZookeeperDirectState" should "respond appropriately when fetch() can't reach zookeeper" in {
+
+    val zkServer = new TestingServer(9011, true)
+    val state: ZookeeperDirectState = createDirectState(serverList = zkServer.getConnectString)
+
+    val name = randomString(20)
+    val data = randomBytes(20)
+    val newVar = state.fetch(name).value.mutate(data)
+    state.store(newVar).value
+    state.fetch(name).value.value() should equal(data)
+
+    zkServer.stop()
+    val f = state.fetch(name).toFuture
+
+    f onComplete {
+      case Success(variable)             => fail("Should not have returned a variable when zk server is down")
+      case Failure(ex: TimeoutException) => println(s"Got the expected error: $ex")
+      case Failure(ex)                   => fail("A TimeoutException should have been thrown")
+    }
+
+    Await.ready(f, 3.seconds)
+  }
+
+  it should "respond appropriately when store() can't reach zookeeper" in {
+
+    val zkServer = new TestingServer(9012, true)
+    val state: ZookeeperDirectState = createDirectState(serverList = zkServer.getConnectString)
+
+    val name = randomString(20)
+    val data = randomBytes(20)
+    val newVar = state.fetch(name).value.mutate(data)
+    state.store(newVar).value
+    val retrievedVariable = state.fetch(name).value
+    retrievedVariable.value() should equal(data)
+    val updatedVariable = retrievedVariable.mutate("Hi There".getBytes)
+
+    zkServer.stop()
+    val f = state.store(updatedVariable).toFuture
+
+    f onComplete {
+      case Success(names)                => fail("Should not have returned names when zk server is down")
+      case Failure(ex: TimeoutException) => println(s"Got the expected error: $ex")
+      case Failure(ex)                   => fail("A TimeoutException should have been thrown")
+    }
+
+    Await.ready(f, 3.seconds)
+  }
+
+  it should "respond appropriately when expunge() can't reach zookeeper" in {
+
+    val zkServer = new TestingServer(9013, true)
+    val state: ZookeeperDirectState = createDirectState(serverList = zkServer.getConnectString)
+
+    val name = randomString(20)
+    val data = randomBytes(20)
+    state.store(state.fetch(name).value.mutate(data)).value
+    val retrievedVariable = state.fetch(name).value
+    retrievedVariable.value() should equal(data)
+
+    zkServer.stop()
+    val f = state.expunge(retrievedVariable).toFuture
+
+    f onComplete {
+      case Success(names)                => fail("Should not have returned names when zk server is down")
+      case Failure(ex: TimeoutException) => println(s"Got the expected error: $ex")
+      case Failure(ex)                   => fail("A TimeoutException should have been thrown")
+    }
+
+    Await.ready(f, 3.seconds)
+  }
+
+  it should "respond appropriately when names() can't reach zookeeper" in {
+
+    val zkServer = new TestingServer(9014, true)
+    val state: ZookeeperDirectState = createDirectState(serverList = zkServer.getConnectString)
+
+    zkServer.stop()
+    val f = state.names.toFuture
+
+    f onComplete {
+      case Success(names)                => fail("Should not have returned names when zk server is down")
+      case Failure(ex: TimeoutException) => println(s"Got the expected error: $ex")
+      case Failure(ex)                   => fail("A TimeoutException should have been thrown")
+    }
+
+    Await.ready(f, 3.seconds)
+  }
+
+  def ensureZookeeperWorks(connectString: String) = {
+    val c = CuratorFrameworkFactory.newClient(connectString, 2000, 2000, new RetryNTimes(0, 0))
+    c.start()
+    c.blockUntilConnected(2, TimeUnit.SECONDS)
+    c.create().forPath("/Greetings")
+    c.setData().forPath("/Greetings", "Hi".getBytes)
+  }
+
+  implicit class Unwrapping[A](j: JFuture[A]) {
+    def toFuture: Future[A] = j.asInstanceOf[ScalaFutureSupport[A]].future
+  }
 }
 
 trait JFutureValues {
@@ -66,7 +159,7 @@ trait JFutureValues {
     */
   implicit class JFutureConversion[A](a: JFuture[A]) {
 
-    val timeout = 10.seconds
+    val timeout = 5.seconds
 
     def value: A = a match {
       case f: ScalaFutureSupport[A] => Await.result[A](f.future, timeout)
@@ -79,7 +172,31 @@ trait JFutureValues {
 /**
   * Fairly tests any implementation of State
   */
-trait StateTesting extends Matchers with RandomTestUtils with JFutureValues { self: FlatSpec =>
+trait StateTesting extends Matchers
+    with RandomTestUtils
+    with ZookeeperTestServerSupport
+    with JFutureValues { self: FlatSpec =>
+
+  def znode = "/" + randomString(10)
+  val timeout = 5.seconds
+
+  // Some useful utils for creating either type of Zookeeper State
+  def createJNIState(node: String = znode) = {
+    new ZooKeeperState(testZKServerList, timeout.toSeconds, SECONDS, node)
+  }
+
+  def createDirectState(node: String = znode, serverList: String = testZKServerList) = {
+    val mills = timeout.toMillis.toInt
+    val client = CuratorFrameworkFactory.newClient(serverList, mills, mills, new RetryNTimes(0, 0))
+    client.start()
+    client.blockUntilConnected(5, TimeUnit.SECONDS)
+    new ZookeeperDirectState(node, client, scala.concurrent.ExecutionContext.Implicits.global)
+  }
+
+  def dupe(state: State): State = state match {
+    case x: ZooKeeperState       => createJNIState()
+    case x: ZookeeperDirectState => createDirectState()
+  }
 
   /**
     *  Test a generic State implementation
@@ -119,6 +236,8 @@ trait StateTesting extends Matchers with RandomTestUtils with JFutureValues { se
       val data = randomBytes(20)
       val version1 = storeData(name, data)
       val version2 = store(version1.mutate("Hi!".getBytes))
+      version2 should not be null
+      version2.value should not be null
       new String(version2.value()) should equal("Hi!")
     }
 
@@ -147,21 +266,31 @@ trait StateTesting extends Matchers with RandomTestUtils with JFutureValues { se
       fetch(name).value().size should equal(0)
     }
 
-    it should "ensure that names will include stored entries" in {
+    it should "ensure that names() will include stored entries" in {
       val name = randomString(20)
       val variable = storeData(name, "Hi There".getBytes)
       names should contain(name)
     }
 
-    it should "ensure that names will not include non-existent entries" in {
+    it should "ensure that names() will not include non-existent entries" in {
       val name = randomString(20)
       names should not contain name
     }
 
-    it should "ensure that names will not include fetched-but-not-stored entries" in {
+    it should "ensure that names() will not include fetched-but-not-stored entries" in {
       val name = randomString(20)
       fetch(name)
       names should not contain name
+    }
+
+    it should "support fetch() when the znode doesn't exist" in {
+      val s = dupe(state)
+      s.fetch("blah").value should not be (null)
+    }
+
+    it should "support store() when the znode doesn't exist" in {
+      val s = dupe(state)
+      s.store(s.fetch("howdy").value.mutate("Yo".getBytes)).value should not be (null)
     }
 
   }
@@ -188,11 +317,11 @@ trait StateTesting extends Matchers with RandomTestUtils with JFutureValues { se
 }
 
 object ZookeeperTestServer {
-  val server = new TestingServer()
+  lazy val server = new TestingServer()
 }
 
 trait ZookeeperTestServerSupport {
-  val testZKServerList: String = ZookeeperTestServer.server.getConnectString
+  lazy val testZKServerList: String = ZookeeperTestServer.server.getConnectString
 }
 
 trait RandomTestUtils {
@@ -205,3 +334,4 @@ trait RandomTestUtils {
     b
   }
 }
+
