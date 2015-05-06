ecd962fc9d851a0aa9e9a8bd9afd996818a0d90f
Dario Rexin <dario.rexin@r3-tech.de>
2015-05-06 14:46:19 +0200

    update mesos dependency to 0.22.1

--- project/build.scala
+++ project/build.scala
@@ -69,7 +69,7 @@ object Dependencies {
 object Dependency {
   object V {
     // runtime deps versions
-    val Mesos = "0.22.0"
+    val Mesos = "0.22.1"
 
     // test deps versions
     val ScalaTest = "2.2.1"
