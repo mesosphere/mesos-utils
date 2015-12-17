8c6484892f760db5d2eff0e5508f854fd43e3c2b
Matthias Veit <matthias_veit@yahoo.de>
2015-12-17 09:55:30 +0100

    Bump mesos dependency to 0.26.0

--- project/build.scala
+++ project/build.scala
@@ -69,7 +69,7 @@ object Dependencies {
 object Dependency {
   object V {
     // runtime deps versions
-    val Mesos = "0.25.0"
+    val Mesos = "0.26.0"
 
     // test deps versions
     val ScalaTest = "2.2.1"
