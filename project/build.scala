ac4223f1297a24ec99ab6aece166b59c8a4ac03d
Gastón Kleiman <gaston@mesosphere.io>
2016-06-08 12:47:12 +0200

    Bump Mesos version and version.sbt to 1.0.0-rc1

--- project/build.scala
+++ project/build.scala
@@ -69,7 +69,7 @@ object Dependencies {
 object Dependency {
   object V {
     // runtime deps versions
-    val Mesos = "0.28.0"
+    val Mesos = "1.0.0-rc1"
 
     // test deps versions
     val ScalaTest = "2.2.1"
