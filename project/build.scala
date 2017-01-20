59f15cb0f9e25d0df4d2ab1876e7aaa2315455bf
Matthias Veit <matthias_veit@yahoo.de>
2017-01-20 13:34:39 +0100

    Use bugfix version for 1.0.2

--- project/build.scala
+++ project/build.scala
@@ -69,7 +69,7 @@ object Dependencies {
 object Dependency {
   object V {
     // runtime deps versions
-    val Mesos = "1.0.1"
+    val Mesos = "1.0.2"
 
     // test deps versions
     val ScalaTest = "2.2.1"
